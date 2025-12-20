2d73b8a5a527476885f47c8a75b4cc9c37f29d67
David Garcia <bytesWright@isdavid.com>
2025-12-20 01:15:13 -0700

    Update docs and refactor rendering pipeline for multi-worker support
    - Enhanced README with additional setup instructions and updated links.
    - Refactored rendering logic to introduce multi-worker rendering pipeline, ensuring better distribution of rendering tasks.
    - Replaced `root` dependency with `path_from_root` for path logic consistency.
    - Adjusted `render_bulk` to handle task partitioning and error logging per worker.
    - Minor fixes to annotation-related documentation links.
    - Removed unnecessary environment dependencies in `e-pip-packages.env`.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Overview
 
-This project generates synthetic images of credit cards for training machine learning models, specifically using the
+This project generates synthetic images of credit cards for training machine learning models. We use the
 YOLOv8 object detection model. The images are generated using a Blender file that runs a Python script to create
 realistic scenes with varied conditions. The generated images are of size 800x800 in JPG format and are accompanied by
 the necessary annotation files for YOLOv8 and Hugging Face.
@@ -16,27 +16,30 @@ the necessary annotation files for YOLOv8 and Hugging Face.
 - **Annotation Files**: Generates annotation files compatible with YOLOv8 and Hugging Face in JSONL format.
 - **Card Background**: Generates different types of images to dress the credit card using Python.
 
-# Project Setup Guide for Credit Card Detection Dataset Generation
+# Setup Guide
 
 This guide outlines the streamlined process for setting up the project using Blender, which includes automatic
 environment creation and package installation:
 
-1. **Install Anaconda**:
+1. **Install Blender**:
+    - Download and install Blender from the official [Blender website](https://www.blender.org/download/).
+
+2. **Install Anaconda**:
     - Download and install Anaconda from [Anaconda's website](https://www.anaconda.com/download). Anaconda is necessary
       to manage the environments and dependencies for the project.
 
-2. **Clone the Repository**:
+3. **Clone the Repository**:
     - Clone the project using the command:
       ```
       git clone https://github.com/bytesWright/creditCardDetectionDSG.git
       ```
 
-3. **Initialize the Environment with Blender**:
+4. **Initialize the Environment with Blender**:
     - Open the Blender file named `generator.blend` and run the script `RUN-ME-FIRST-RUN-ME-ONCE`. This script
       automatically creates a new Anaconda environment tailored specifically for this project and installs the necessary
       dependencies using pip to ensure compatibility with Blender's Python interpreter.
 
-4. **Verify the Environment Setup**:
+5. **Verify the Environment Setup**:
     - After the initial environment setup, execute the `RUN-ME-SECOND` script located in the same Blender file. This
       script checks if the PIL library, a crucial dependency, is correctly installed in the Conda environment. This
       verification ensures that the environment is properly configured for synthetic dataset generation.
@@ -80,12 +83,12 @@ a detailed list of these scripts:
 
 ## YOLOv8
 
-The annotation files for YOLOv8 are generated as per the YOLOv8 dataset requirements. Each image will have a
+The annotation files for YOLOv8 are generated as per the [YOLOv8 dataset requirements](https://docs.ultralytics.com/datasets/detect/). Each image will have a
 corresponding .txt file with the bounding box coordinates.
 
 ## Hugging Face
 
-The annotation files for Hugging Face are generated in JSONL format as per the Hugging Face image dataset documentation.
+The annotation files for Hugging Face are generated in JSONL format as per the [Hugging Face image dataset documentation](https://huggingface.co/docs/datasets/en/image_dataset).
 
 # Links
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,14 @@
+import datetime
+import math
 import os
 import time
 
 from scripts.blender.query.get_scene_and_camera import get_scene_and_camera
-from scripts.blender.render.render_card_side import render_card_side, query_root_path
+from scripts.blender.render.render_card_side import render_card_side
+from scripts.common.file import path_from_root, append_line_to_file
 
 
 def render_post_validation():
-    root = query_root_path()
-
     data_set_name = "post_validation"
     distribution = [
         {
@@ -18,7 +19,7 @@ def render_post_validation():
                         {
                             "name": "draw_random_image",
                             "parameters": {
-                                "container_path": os.path.join(root, "assets/images/real_cards/post_validation_source")
+                                "container_path": path_from_root("./assets/images/real_cards/post_validation_source")
                             }
                         }
                     ],
@@ -34,58 +35,74 @@ def render_post_validation():
 def render_data_set():
     data_set_name = "testX"
     distribution = [
-        {"name": "train", "starting": 0, "limit": 20},
-        {"name": "val", "starting": 0, "limit": 20},
-        {"name": "test", "starting": 0, "limit": 20}
+        {"name": "train", "starting": 123, "limit": 1200},
+        # {"name": "val", "starting": 0, "limit": 800},
+        # {"name": "test", "starting": 0, "limit": 800}
     ]
 
-    render_bulk(data_set_name, distribution)
+    render_bulk(data_set_name, distribution, render_worker=2)
 
 
 def render_test_front(samples_limit=5):
-    data_set_name = "TEST"
+    data_set_name = "TEST-FRONT"
     distribution = [
         {"name": "train", "starting": 0, "limit": samples_limit},
     ]
+
     render_bulk(data_set_name, distribution, restrict_side="front")
 
 
 def render_test_back(samples_limit=5):
-    data_set_name = "TEST"
+    data_set_name = "TEST-BACK"
     distribution = [
         {"name": "train", "starting": 0, "limit": samples_limit},
     ]
     render_bulk(data_set_name, distribution, restrict_side="back")
 
 
-def render_bulk(data_set_name, distribution, restrict_side="both"):
-    root = query_root_path()
+def render_bulk(data_set_name, distribution, restrict_side="both", render_worker=None, workers=3):
+    workers_flag_path = path_from_root(f"./project/")
+    workers_count = len([wff for wff in os.listdir(workers_flag_path) if wff.endswith(".wrk")])
+
+    worker_flag_path = path_from_root(f"./project/worker_{workers_count}.wrk")
+    append_line_to_file(worker_flag_path, "")
+
+    render_worker = workers_count
+
     scene, camera = get_scene_and_camera()
 
     print(f"Rendering {data_set_name}.")
 
-    elements_remaining = sum(value["limit"] - value["starting"] for value in distribution)
-    total_items = elements_remaining
+    dataset_total_items = sum(value["limit"] - value["starting"] for value in distribution)
 
     start_time = time.time()
     total_counter = 0
+    render_counter = 0
+
+    batch_size = math.ceil(dataset_total_items / workers)
+    skip_until = render_worker * batch_size
+    stop_on = (render_worker + 1) * batch_size
+
+    elements_remaining = batch_size
 
     for bucket_parameters in distribution:
         bucket_name = bucket_parameters["name"]
-        output_path = os.path.join(root, "output", data_set_name)
+        output_path = path_from_root("./output", data_set_name)
         starting = bucket_parameters["starting"]
         limit = bucket_parameters["limit"]
-        total_to_render = limit - starting
 
-        for i in range(starting, bucket_parameters["limit"]):
+        for i in range(starting, limit):
+            total_counter += 1
+
+            if total_counter < skip_until:
+                continue
+            if total_counter > stop_on:
+                continue
 
             print()
             print("-----------")
-            print(f"    Rendering bucket [{bucket_name}]. Render item {i}")
-            print(f"    Range: {starting}-{limit}. Total bucket items {total_to_render}")
+            print(f"    Rendering bucket [{bucket_name}]. Render item {i}. Worker {render_worker}")
             print()
-            elements_remaining -= 1
-            total_counter += 1
 
             render_start_time = time.time()
 
@@ -93,18 +110,30 @@ def render_bulk(data_set_name, distribution, restrict_side="both"):
             if (restrict_side == "both" and i % 2 == 0) or restrict_side == "back":
                 card_side = "back"
 
-            render_card_side(root, output_path, bucket_parameters, i, scene, camera, card_side)
+            while True:
+                try:
+                    render_card_side(output_path, bucket_parameters, i, scene, camera, card_side)
+                    break
+                except Exception as e:
+                    print(f"Error rendering.")
+                    print(e)
+                    append_line_to_file(f"./worker{render_worker}", str(e))
+
+            render_counter += 1
+            elements_remaining -= 1
 
             render_end_time = time.time()
             render_elapsed_time = render_end_time - render_start_time
-            time_remaining_in_hours = (render_elapsed_time * elements_remaining) / 60.0 / 60.0
+            time_remaining = datetime.timedelta(seconds=render_elapsed_time * elements_remaining)
 
             print(f"    Render time: {round(render_elapsed_time, 2)}s.")
-            print(f"    Total time remaining {int(time_remaining_in_hours)}h:{round((time_remaining_in_hours % 1) * 60, 2)}m")
-            print(f"    Rendered {total_counter} of {total_items} total items. {elements_remaining} total items remaining.")
+            print(f"    Total time remaining {time_remaining}")
+            print(f"    Rendered {render_counter} of {batch_size} total items. {elements_remaining} total items remaining.")
+
+    os.remove(worker_flag_path)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
 
     print(f"\n\n!!!DONE!!!")
-    print(f"Total elapsed time: {elapsed_time}s")
+    print(f"Total elapsed time: {round(elapsed_time / 60 / 60, 3)}h")
