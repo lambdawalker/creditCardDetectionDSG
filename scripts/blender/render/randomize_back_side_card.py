3e1f3b689a342ecefb526d8dcba6f96dc8c778bd
David Garcia <bytesWright@isdavid.com>
2025-12-20 01:13:43 -0700

    Refactor rendering pipeline: remove `root` dependency, centralize path logic with `path_from_root`.

--- scripts/blender/render/randomize_back_side_card.py
+++ scripts/blender/render/randomize_back_side_card.py
@@ -1,27 +1,29 @@
 import bpy
-from scripts.blender.spatial.compute_pixel_bounding_box import compute_vectors_pixel_bounding_box
+
 from scripts.blender.images.assign_image_to_texture import assign_image_to_texture
 from scripts.blender.material.update import randomize_material_parameters_from_template
-from scripts.blender.spatial.compute_3d_bbox_positions import compute_3d_bbox_positions
 from scripts.blender.render.spatial import randomize_card_position_and_rotation, calculate_center_and_plane_size
+from scripts.blender.spatial.compute_3d_bbox_positions import compute_3d_bbox_positions
+from scripts.blender.spatial.compute_pixel_bounding_box import compute_vectors_pixel_bounding_box
+from scripts.common.file import path_from_root
 from scripts.templates.render_template import render_template
 
 
-def randomize_band(root):
-    file_path = f"{root}/assets/materials_randomization/Band.json"
+def randomize_band():
+    file_path = path_from_root("./assets/materials_randomization/Band.json")
     randomize_material_parameters_from_template(file_path)
 
 
-def randomize_back_side_card(root, card_object, **side_parameters):
+def randomize_back_side_card(card_object, **side_parameters):
     if isinstance(card_object, str):
         card_object = bpy.data.objects.get(card_object)
 
     randomize_card_position_and_rotation(card_object, side="back")
     bpy.context.view_layer.update()
 
-    randomize_band(root)
+    randomize_band()
     image, relative_bounding_boxes, card_log = render_template(
-        root_path=root, width=900, height=540, side="back", **side_parameters
+        width=900, height=540, side="back", **side_parameters
     )
 
     assign_image_to_texture("Back", "Color", image)
@@ -54,6 +56,6 @@ def randomize_back_side_card(root, card_object, **side_parameters):
     return data, card_log
 
 
-def randomize_card_back_plastic(root):
-    file_path = f"{root}/assets/materials_randomization/Back.json"
+def randomize_card_back_plastic():
+    file_path = path_from_root("./assets/materials_randomization/Back.json")
     randomize_material_parameters_from_template(file_path)
